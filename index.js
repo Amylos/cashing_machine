11f3c4bac4f0ebc3532501d059e8c60eed8d0bc2
Aboubakr <aboubakr.zennir@gmail.com>
2025-10-28 14:26:32 +0100

    feat: add calculation logic and api endpoint

--- index.js
+++ index.js
@@ -2,19 +2,73 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { connectDb } from "./config/db.js";
+import ExchangeService from "./services/ExchangeService.js";
+import ClientWallet from "./models/ClientWalletSchema.js";
+import CalculateChangeService from "./services/CalculateChangeService.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+await connectDb();
+
+ExchangeService.createExchange();
+
+console.log(
+  `Total de la caisse actuellement : ${ExchangeService.getTotalExchange(
+    await ExchangeService.getExchange()
+  )}`
+);
 
 app.get("/", (req, res) => {
   const user = { name: "Aboubakr", age: 25 };
   res.render("index", { user });
 });
 
-await connectDb();
+// CLIENT
+app.post("/api/payment", async (req, res) => {
+  const { amount, exchange, bigBills } = req.body;
+
+  try {
+    // 1️⃣ Calculer le rendu
+    const result = await CalculateChangeService.calculateChange(
+      amount,
+      exchange,
+      bigBills
+    );
+
+    console.log(result);
+
+    if (!result.success) {
+      return res.status(400).json(result);
+    }
+
+    console.log("update");
+
+    // 2️⃣ Mettre à jour la caisse existante
+    const caisseDoc = await ExchangeService.getExchange();
+    Object.assign(caisseDoc, result.updatedCaisse);
+    await caisseDoc.save();
+
+    // 3️⃣ Créer une nouvelle ligne ClientWallet
+    const clientWallet = new ClientWallet({
+      amount,
+      exchange,
+      bigBills,
+    });
+    await clientWallet.save();
+
+    // 4️⃣ Renvoyer le rendu au client
+    res.render("change", { changeToGive: result.changeToGive });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`✅ Server is running on port ${process.env.PORT}`);
